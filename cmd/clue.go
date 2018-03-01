21d7209896ff037c58e8ebb82b4ed891c1ac4442
Spencer Williams <s@spencerenglish.com>
2018-03-01 12:00:48 -0800

    help text

--- cmd/clue.go
+++ cmd/clue.go
@@ -10,13 +10,14 @@ import (
 // clueCmd represents the clue command
 var clueCmd = &cobra.Command{
 	Use:   "clue",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Lists words matching a given clue.",
+	Long: `Lists words matching a given clue.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+A clue can contain any information about
+
+- length
+- the set of letters available to make the word
+- the existing "fill", or positions of known letters in the word`,
 	RunE: clue,
 }
 
